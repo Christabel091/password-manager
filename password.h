0a4345257d5484dacb7bd036ed112cff19c6599e
Christabel <obinwosugosiorah@gmail.com>
2024-05-22 00:27:54 -0400

    wrote the blueprint for the class user

---
+++ password.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include "user_password.h"
+
+class User{
+    std::string user_name;
+    std::vector<eachPassword> passswords;
+public:
+    User(std::string );
+    void password_suggester();
+    void change_password();
+};
