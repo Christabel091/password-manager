5fda626093d2c9c3f196f98a4d2bc1705c159366
Christabel <obinwosugosiorah@gmail.com>
2024-06-04 19:04:38 -0500

    added new statements

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
-from  user import User
+from user import User
 
 def main():
-    #getting inputs
+    # Getting user input
     print("WELCOME USER!!!!")
     user_name = input("What is your name: ")
     user = User(user_name)
-    password = input("Enter the password to save today: ")
-    platform = input("What is the platform for the passowrd")
-    user.set_platform(platform)
-    pw_valid=user.validate_pw(password)
-    if pw_valid != True:
-        print("\n\npassword is not a valid password and has low strength")
-        print("You can validate the password or i can suggest something new ")
-        action = input("say YES to suggest and NO to validate ")
-        while action != "YES" and action != "NO":
-            print("wrong input")
-            password=input('Enter your password:')
-        if action == "YES":
-            sug = user.password_suggester()
-        elif action == "NO":
-            password=input('Enter your password:')
-            pw_valid=user.validate_pw(password) 
-    if pw_valid == True:
-        user.set_password(password)
-    else:
-        print("failed")
-        #implement suggestion function later
     
-    
-
+    while True:
+        print("\n Welcome " + user_name)
+        print("1. Save Password")
+        print("2. Create or Suggest Password")
+        print("3. Analyze Password")
+        print("4. Exit")
         
+        choice = input("Enter your choice (1-4): ")
+        
+        if choice == '1':
+            platform = input("What is the platform for the password: ")
+            password = input("Enter the password to save: ")
+            pw_valid = user.validate_pw(password)
+            if not pw_valid:
+                print("Password is not valid.")
+                continue
+            print("checking to see if reused")
+            reused = user.password_used(password)
+            if reused:
+                print("it would be a good idea to use another password, password has been used. ")
+                move = input("type u to use anyway and p to try againy")
+                move = move.lower()
+                if move == "u":
+                    user.set_platform(platform)
+                    user.set_password(password)
+                    continue
+                else:
+                    continue
+            user.set_platform(platform)
+            user.set_password(password)
+        elif choice == '2':
+            platform = input("what platform is the suggested password for: ")
+            user.set_platform(platform)
+            sug = user.password_suggester()
+            reused = user.password_used(sug)
+            if reused:
+                print("it would be a goo idea to use another password, password has been used. ")
+                move = input("type u to use anyway and p to try againy")
+                move = move.lower()
+                if move == "u":
+                    user.set_platform(platform)
+                    user.set_password(password)
+                else:
+                    continue
+            user.set_password(sug)
+            print("Password suggested:", sug)
+        elif choice == '3':
+            user.analyze_strength()
+        elif choice == '4':
+            print("Exiting the program.")
+            print("BYEEEE!!! " + user_name)
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
+
 if __name__ == "__main__":
     main()
--- user.py
+++ user.py
@@ -1,27 +1,36 @@
+import random
+import string
+import re
+
 class User:
     def __init__(self, name):
         self.user_name = name
         self.user_password = None
-        self.user_arr = [self.user_name]
         self.password_arr = []
+        self.pform_arr = []
         self.pform = None
 
     def set_password(self, password):
         # Function to store the password
         self.user_password = password
         self.password_arr.append(self.user_password)
-        self.user_arr.append(self.password_arr)
         print(f"Password set for {self.user_name}")
 
     def password_suggester(self):
         # Function to suggest passwords
-        suggested_passwords = ["password123", "1234abcd", "securePass1!"]
-        print(f"Suggested passwords for {self.user_name}: {', '.join(suggested_passwords)}")
-        return True
+        # Define the character set to include letters, digits, and special characters
+        char_set = string.ascii_letters + string.digits + string.punctuation
+        length = 12
+
+        # Generate a random string of the specified length
+        random_string = ''.join(random.choice(char_set) for _ in range(length))
+        print(random_string)
+        print("password suggested")
+        return random_string
 
     def set_platform(self, platform):
         self.pform = platform
-        self.user_arr.append(self.pform)
+        self.pform_arr.append(self.pform)
         print(f"Platform set to {self.pform} for {self.user_name}")
 
     def change_password(self, new_password):
@@ -66,6 +75,66 @@ class User:
             return False
         else:
             return True
-    def get_arr(self):
-        print(self.user_arr)
+        
                 
+    def password_used(self, password):
+        for pw in self.password_arr:
+            if pw == password:
+                return True
+        return False
+
+
+
+    def analyze_strength(self):
+        for pw in self.password_arr:
+            weights = {
+                'length': 0.2,
+                'uppercase': 0.2,
+                'lowercase': 0.2,
+                'digit': 0.2,
+                'special_char': 0.1,
+                'common_pattern': 0.1,
+            }
+            
+            length_criteria = len(pw) >= 12
+            uppercase_criteria = bool(re.search(r'[A-Z]', pw))
+            lowercase_criteria = bool(re.search(r'[a-z]', pw))
+            digit_criteria = bool(re.search(r'[0-9]', pw))
+            special_char_criteria = bool(re.search(r'[!@#$%^&*(),.?":{}|<>]', pw))
+            
+            common_patterns = ["123456", "password", "qwerty", "admin", "letmein", self.user_name, "Password12#",(self.user_name + "123#") ]
+            common_pattern_criteria = not any(pattern in pw.lower() for pattern in common_patterns)
+            
+            # Calculate strength
+            strength = (
+                weights['length'] * length_criteria +
+                weights['uppercase'] * uppercase_criteria +
+                weights['lowercase'] * lowercase_criteria +
+                weights['digit'] * digit_criteria +
+                weights['special_char'] * special_char_criteria +
+                weights['common_pattern'] * common_pattern_criteria
+            )
+            
+            # Convert strength to percentage
+            strength_percentage = int(strength * 100)
+            
+            # Feedback
+            feedback = []
+            if not length_criteria:
+                feedback.append("Password should be at least 12 characters long.")
+            if not uppercase_criteria:
+                feedback.append("Password should include at least one uppercase letter.")
+            if not lowercase_criteria:
+                feedback.append("Password should include at least one lowercase letter.")
+            if not digit_criteria:
+                feedback.append("Password should include at least one digit.")
+            if not special_char_criteria:
+                feedback.append("Password should include at least one special character.")
+            if not common_pattern_criteria:
+                feedback.append("Password should not contain common patterns or easily guessable sequences.")
+            
+            print(f"Password: {pw}")
+            print(f"Strength: {strength_percentage}%")
+            for f in feedback:
+                print(f)
+            print()
